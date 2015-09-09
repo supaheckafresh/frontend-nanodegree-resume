ea232b3664e48671d873357b69aaa6fb382a308f
supaheckafresh <neilstrain@gmail.com>
2015-09-09 10:47:45 -0700

    Display bio info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,34 +1,39 @@
 var name = "Neil Strain";
 var role = "Web Developer";
-var contactInfo = {
-	"email" : "neilstrain@gmail.com",
-	"mobile" : "(562)810-2501",
-	"github" : "blank",
-	"location" : "Exeter, CA"
-};
-var pictureUrl = "https://s3.amazonaws.com/teacher-files-musicteachershelper-com/21871/neil.jpg";
+var email = "neilstrain@gmail.com";
+var mobile = "(562)810-2501";
+var github = "https://github.com/supaheckafresh";
+// var location = "Exeter, CA";
+var bioPic = "https://s3.amazonaws.com/teacher-files-musicteachershelper-com/21871/neil.jpg";
 var welcomeMsg = "hello and welcome to my humble abode.";
-var skills = {"awesomeness", 
-			"discipline", 
-			"patience", 
-			"management", 
-			"sales"};
+var skills = ["awesomeness", "discipline", "patience", "management", "sales"];
 
 var bio = {
 	"name" : name,
 	"role" : role,
-	"contactInfo" : contactInfo,
-	"pictureUrl" : pictureUrl,
+	"email" : email,
+	"mobile" : mobile,
+	"github" : github,
+	// "location" : location,
+	"bioPic" : bioPic,
 	"welcomeMsg" : welcomeMsg,
 	"skills" : skills
 };
 
-//TODO : get this working
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-var formattedEmail = HTMLemail.replace("%data%", contactInfo.email);
-
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedbioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills.join(", "));
+var formattedEmail = HTMLemail.replace("%data%", bio.email);
+var formattedMobile = HTMLmobile.replace("%data%", bio.mobile);
+var formattedGithub = HTMLgithub.replace("%data%", bio.github);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
-$("#footerContacts").prepend(formattedEmail);
+$("#header").prepend(formattedbioPic);
+$("#header").append(formattedWelcomeMsg);
+$("#header").append(formattedSkills);
+$("#footerContacts").append(formattedEmail);
+$("#footerContacts").append(formattedMobile);
+$("#footerContacts").append(formattedGithub);

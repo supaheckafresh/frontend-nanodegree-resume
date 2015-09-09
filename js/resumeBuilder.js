a3fe0d13d12bde8adc897e56c7361159710a7c94
supaheckafresh <neilstrain@gmail.com>
2015-09-08 21:36:33 -0700

    Add bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,34 @@
 var name = "Neil Strain";
 var role = "Web Developer";
+var contactInfo = {
+	"email" : "neilstrain@gmail.com",
+	"mobile" : "(562)810-2501",
+	"github" : "blank",
+	"location" : "Exeter, CA"
+};
+var pictureUrl = "https://s3.amazonaws.com/teacher-files-musicteachershelper-com/21871/neil.jpg";
+var welcomeMsg = "hello and welcome to my humble abode.";
+var skills = {"awesomeness", 
+			"discipline", 
+			"patience", 
+			"management", 
+			"sales"};
 
+var bio = {
+	"name" : name,
+	"role" : role,
+	"contactInfo" : contactInfo,
+	"pictureUrl" : pictureUrl,
+	"welcomeMsg" : welcomeMsg,
+	"skills" : skills
+};
+
+//TODO : get this working
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedEmail = HTMLemail.replace("%data%", contactInfo.email);
+
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$("#footerContacts").prepend(formattedEmail);

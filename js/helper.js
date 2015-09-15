5c44a527d91fff9b3cb129f85a8b3de99765caae
supaheckafresh <neilstrain@gmail.com>
2015-09-14 21:54:56 -0700

    Add split() to name input on inName() to remove leading and trailing white space.

--- js/helper.js
+++ js/helper.js
@@ -63,7 +63,7 @@ var googleMap = '<div id="map"></div>';
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
 function inName(name) {
-  var iName = name.split(" ");
+  var iName = name.trim().split(" ");
   iName[0] = iName[0][0].toUpperCase() +
     iName[0].slice(1).toLowerCase();
   iName[1] = iName[1].toUpperCase();

1561a8948cda850c7be5b83a4ecdada10ba70206
supaheckafresh <neilstrain@gmail.com>
2015-09-24 16:32:35 -0700

    iphone.

--- js/helper.js
+++ js/helper.js
@@ -14,8 +14,8 @@ var HTMLlocation = '<li class="flex-box"><span class="purple-text">location</spa
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<div><h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul></div>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskills = '<li id="skill" class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#" target="_blank">%data%';
